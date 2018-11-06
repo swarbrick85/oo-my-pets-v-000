ea7e31fdf9c82c3f19f76dbdbbd91e5ed8e26396
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2018-11-06 18:01:40 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,27 @@
 class Owner
-  # code goes here
+  
+  @@owners = []
+  attr_reader :species 
+  attr_accessor :name 
+  
+  def initialize(species)
+    @species = species 
+    @@owners << self 
+    
+  end 
+  
+  def say_species
+    return "I am a #{@species}."
+  end 
+  
+  def self.all 
+    
+    @@owners.size 
+    return @@owners
+  end 
+  
+  
+  def self.reset_all 
+    Owner.all = nil 
+  end 
 end
