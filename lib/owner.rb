b30ba0c402559a35f19c90830e1b9665af5bd100
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2018-11-06 18:48:45 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -34,6 +34,12 @@ class Owner
     end 
   end 
   
+  def play_with_cats
+    pets[:cats].each do |cat|
+      cat.mood = "happy"
+    end 
+  end 
+  
   def self.all 
     return @@owners
   end 
