93ffe26b605366de49ac7921c8d60e341ac15a74
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2018-11-06 18:09:20 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -2,18 +2,21 @@ class Owner
   
   @@owners = []
   attr_reader :species 
-  attr_accessor :name 
+  attr_accessor :name, :pets  
   
   def initialize(species)
     @species = species 
     @@owners << self 
+    @pets = {a:
   end 
   
   def say_species
     return "I am a #{@species}."
   end 
   
-  
+  def pets 
+    
+  end 
   
   def self.all 
     return @@owners
